4f7f25fed98fa5707af2490fbaf5ec0907e45b30
Ryan Mulcahy <ryan.mulcahy@code42.com>
2015-09-12 18:21:19 -0500

    Failing test for 57

--- test/four_clojure/fifty_seven_test.clj
+++ test/four_clojure/fifty_seven_test.clj
@@ -1,5 +1,8 @@
 (ns four-clojure.fifty-seven-test
-  (:require [expectations
-             four-cloj]))
+  (:require [expectations :refer [expect]]
+            [four-clojure.fifty-seven :refer :all]))
 
-(excpect [3 2 1] ((__ rest reverse) [1 2 3 4]))
+(expect [3 2 1] ((my-comp rest reverse) [1 2 3 4]))
+(expect 5 ((my-comp (partial + 3) second) [1 2 3 4]))
+(expect true ((my-comp zero? #(mod % 8) +) 3 5 7 9))
+(expect "HELLO" ((my-comp #(.toUpperCase %) #(apply str %) take) 5 "hello world"))
