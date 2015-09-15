28f3dcf8a7771e9554284ac18aefe01158f83e46
Ryan Mulcahy <ryan.mulcahy@code42.com>
2015-09-15 12:15:17 -0500

    Solution for 63

--- src/four_clojure/sixty_three.clj
+++ src/four_clojure/sixty_three.clj
@@ -1,4 +1,9 @@
 (ns four-clojure.sixty-three)
 
 (defn my-group-by [f seq]
-  {})
+  (reduce
+   (fn [acc val]
+     (let [x (f val)]
+       (assoc acc x (conj (get acc x []) val))))
+   {}
+   seq))
