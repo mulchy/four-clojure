c0729f1124104014362d91b602892e0a1ece2362
Ryan Mulcahy <ryan.mulcahy@code42.com>
2015-09-18 17:08:02 -0500

    Solution for 69 and 77

---
+++ src/four_clojure/seventy_seven.clj
@@ -0,0 +1,15 @@
+(ns four-clojure.seventy-seven)
+
+(defn anagram? [x y]
+  (= (frequencies x) (frequencies y)))
+
+(defn impl [coll acc]
+  (if (empty? coll)
+    acc
+    (let [current (first coll)
+          anagram-map (group-by #(anagram? current %) (rest coll))]
+      (recur (get anagram-map false) (conj acc (into #{current} (get anagram-map true))))
+    )))
+
+(defn collect-anagrams [coll]
+  (set (filter #(>= (count %) 2) (impl coll []))))
